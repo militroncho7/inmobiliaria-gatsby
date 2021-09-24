605d4dbfd29f8e29e7606f1644b13f50289b0e8b
militroncho7 <vm.moralesruiz@gmail.com>
2021-09-24 09:25:25 +0200

    start project

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a0c27a947bf402c1c6dbf276e89d78ef'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '99976a7d-9c27-4937-9554-856e5b9c7111'
+};
